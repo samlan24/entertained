1a150a60d6945fe5d9f909184ca7fd633a5ea897
samlan24 <muchemionline@gmail.com>
2024-09-28 16:51:48 +0300

    reverting vercel deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: { manifest: true, outDir: './dist'},
-  base:  '/',
   plugins: [react()],
+  build: {
+    outDir: 'dist'
+  }
 })
