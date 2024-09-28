c563211d9f489d9d9d8430102cb1ea7abe9896a8
samlan24 <muchemionline@gmail.com>
2024-09-28 19:21:59 +0300

    changes to gunicorn config file

--- backend/gunicorn.conf.py
+++ backend/gunicorn.conf.py
@@ -1,5 +1,5 @@
 import multiprocessing
 
 bind = "0.0.0.0:5000"  # Bind to all interfaces on port 8000
-workers = multiprocessing.cpu_count() * 2 + 1  # Set the number of workers
+workers = multiprocessing.cpu_count() # Set the number of workers
 timeout = 60  # Set timeout for workers
